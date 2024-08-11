43d56440ab89ed5768f9fcb968ca166fdb447653
LolaKabona <45290965+LolaKabona@users.noreply.github.com>
2024-08-11 19:02:13 +0300

    Update send_html_email.sh
    bash script that will send an email to the victim to simulate a phishing attack

--- send_html_email.sh
+++ send_html_email.sh
@@ -1,8 +1,10 @@
-TO="iamrosemary237@gmail.com"
+#!/bin/bash
+
+TO="a91144187@gmail.com"
 SUBJECT="Security Alert"
 BODY="email_body.html"
 FROM_NAME="Google"
-FROM_EMAIL="iamrosemary237@gmail.com"
+FROM_EMAIL="cyberhera2000@gmail.com"
 FROM="$FROM_NAME <$FROM_EMAIL>"
 
 # Create the email headers and body
@@ -10,7 +12,10 @@ FROM="$FROM_NAME <$FROM_EMAIL>"
   echo "From: $FROM"
   echo "To: $TO"
   echo "Subject: $SUBJECT"
-  echo "Content-Type: text/html"
+  echo "MIME-Version: 1.0"
+  echo "Content-Type: text/html; charset=UTF-8"
+  echo "Content-Transfer-Encoding: 7bit"
   echo
   cat $BODY
 } | msmtp --debug --from=default -t $TO
+
